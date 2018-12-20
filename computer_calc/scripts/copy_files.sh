1a6b15f6bc2104fad3bff53831841e7e5eee5e7e
Thies Lennart Alff <lennart.alff@gmail.com>
2018-12-20 14:51:32 +0100

    Added initial version of raspi_standalone package

---
+++ computer_calc/scripts/copy_files.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# copies raspi_nodes to the raspberry pi
+scp -r ../../computer_calc ubuntu@10.42.0.1:/home/ubuntu/catkin_ws/src
---
+++ raspi_standalone/scripts/copy_files.sh
@@ -0,0 +1,2 @@
+# copies raspi_nodes to the raspberry pi
+scp -r ../../raspi_standalone ubuntu@10.42.0.1:/home/ubuntu/catkin_ws/src
